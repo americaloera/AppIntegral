78f14a234b4ab5d26353b74b29250dc6e6b14352
americaloera <americaloera145@gmail.com>
2023-10-23 17:45:28 -0600

    Agregar archivos de proyecto.

---
+++ App1/App1/Views/AboutPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            Launcher.OpenAsync(new Uri("https://www.utt.edu.mx"));
+        }
+    }
+}
